a3e6b31fd8566fb65d5f09e744ac1e7f145c8c00
EsraYalcin <yalcinaceresra@gmail.com>
2023-12-13 14:33:22 +0100

    added login

--- src/helpers/ToastNotify.js
+++ src/helpers/ToastNotify.js
@@ -1,23 +1,9 @@
- import {toast} from "react-toastify"
- //https://fkhadra.github.io/react-toastify/introduction
+import {toast} from "react-toastify"
+//https://fkhadra.github.io/react-toastify/introduction
 
- export const toastSuccessNotifY=(msg)=>{
+export const toastSuccessNotifY=(msg)=>{
 
-     toast.success(msg, {
-      
-       autoClose: 5000,
-       hideProgressBar: false,
-       closeOnClick: true,
-       pauseOnHover: true,
-       draggable: true,
-       progress: undefined,
-      
-     });
- }
-
-export const toastWarnNotifY=(msg)=>{
-
-    toast.warn(msg, {
+    toast.success(msg, {
      
       autoClose: 5000,
       hideProgressBar: false,
@@ -29,16 +15,30 @@ export const toastWarnNotifY=(msg)=>{
     });
 }
 
+export const toastWarnNotifY=(msg)=>{
+
+   toast.warn(msg, {
+    
+     autoClose: 5000,
+     hideProgressBar: false,
+     closeOnClick: true,
+     pauseOnHover: true,
+     draggable: true,
+     progress: undefined,
+    
+   });
+}
+
 
 export const toastErrorNotifY=(msg)=>{
-    toast.error(msg, {
-      
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    
-    });
+   toast.error(msg, {
+     
+     autoClose: 5000,
+     hideProgressBar: false,
+     closeOnClick: true,
+     pauseOnHover: true,
+     draggable: true,
+     progress: undefined,
+   
+   });
 }
